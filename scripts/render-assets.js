edc5556581aa0a47100a4dd273ac049b4b92c037
Sergey Zelenov <github@zelenov.su>
2024-07-21 11:51:20 +0200

    update assets

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -1,11 +1,25 @@
-'use strict';
-const fs = require('fs');
-const upath = require('upath');
-const sh = require('shelljs');
+"use strict";
+const fs = require("fs");
+const upath = require("upath");
+const sh = require("shelljs");
 
 module.exports = function renderAssets() {
-    const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
-    const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
+    const sourcePath = upath.resolve(
+        upath.dirname(__filename),
+        "../src/assets"
+    );
+    const destPath = upath.resolve(upath.dirname(__filename), "../dist/.");
 
-    sh.cp('-R', sourcePath, destPath)
+    sh.cp(
+        "-R",
+        upath.resolve(upath.dirname(__filename), "../src/robots.txt"),
+        upath.resolve(upath.dirname(__filename), "../dist/.")
+    );
+    sh.cp(
+        "-R",
+        upath.resolve(upath.dirname(__filename), "../src/sitemap.xml"),
+        upath.resolve(upath.dirname(__filename), "../dist/.")
+    );
+
+    sh.cp("-R", sourcePath, destPath);
 };
